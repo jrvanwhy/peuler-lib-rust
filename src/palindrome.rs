04906d9dfa3caa9cc14d95ed8ed5da3adbe6c091
Johnathan Van Why <jrvanwhy@gmail.com>
2015-05-21 16:11:04 -0700

    Add arbitrary-base palindrome checking.

--- src/palindrome.rs
+++ src/palindrome.rs
@@ -1,21 +1,26 @@
-// Function for breaking an unsigned integer into its digits
+// Function for breaking an unsigned integer into its digits in an arbitrary base
 // This is recursive. Deeper recursive levels handle higher
 // digits of the number.
-pub fn digits(num: u32) -> Vec<u8> {
+pub fn digits_base(num: u32, base: u8) -> Vec<u8> {
 	// Compute the last digit and remove it from the number
-	let lastdig: u8 = (num % 10) as u8;
-	let remnum = num / 10;
+	let lastdig: u8 = (num % (base as u32)) as u8;
+	let remnum = num / (base as u32);
 
 	// If only zero remains, we return a vector with just the last digit.
 	// Otherwise, we recurse down
 	if remnum == 0 {
 		vec![lastdig]
 	} else {
-		let mut out = digits(remnum);
+		let mut out = digits_base(remnum, base);
 		out.push(lastdig);
 		out
 	}
 }
+// Function for breaking an unsigned integer into its digits
+pub fn digits(num: u32) -> Vec<u8> {
+	digits_base(num, 10)
+}
+
 
 // Converts a vector of digits into an unsigned integer.
 // This is recursive
@@ -30,10 +35,10 @@ pub fn digits_to_int(mut digs: Vec<u8>) -> u32 {
 	10*digits_to_int(digs) + lastdig
 }
 
-// Checks whether the given unsigned integer is a palindrome
-pub fn is_palindrome(num: u32) -> bool {
-	// Break its digits out into a vector
-	let expanded = digits(num);
+// Checks whether the given unsigned integer is a palindrome in the given base
+pub fn is_palindrome_base(num: u32, base: u8) -> bool {
+	// Break its digits into a vector
+	let expanded = digits_base(num, base);
 
 	// Iterate through the first "half" of the vector
 	for i in (0..expanded.len()/2) {
@@ -43,6 +48,11 @@ pub fn is_palindrome(num: u32) -> bool {
 	}
 
 	true
+}
+
+// Checks whether the given unsigned integer is a palindrome
+pub fn is_palindrome(num: u32) -> bool {
+	is_palindrome_base(num, 10)
 }
 
 // Unit tests for is_palindrome
